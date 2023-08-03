c9643df93c76aeac8d4790db57f5c7464cb88970
wrath1920 <140944200+wrath1920@users.noreply.github.com>
2023-08-03 14:03:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// show new quote
+function newQuote() {
+    loading();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    
+    // check author is null
+    if (!quote.author){
+        authorText.textContent = 'Unknown';
+    }else{
+        authorText.textContent = quote.author;
+    }
+
+    // check quote length 
+    if (quote.text.length > 80){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    //hide loader show text
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+// get quotes from api
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes'
+    try{ 
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    }catch(error){
+
+    }
+    complete();
+}
+
+// tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event listners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+getQuotes();
